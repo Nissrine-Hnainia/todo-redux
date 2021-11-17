7f6eeec0f7f4b33e2b7123c12dd717775a4ec869
Nissrine-Hnainia <hnainianesrine@gmail.com>
2021-11-17 14:14:55 +0100

    restart

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import './App.css';
-import AddTodo from './components/AddTodo';
-import Header from './components/Header';
-import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <AddTodo />
-      <TodoList />
+      hello
     </div>
   );
 }
